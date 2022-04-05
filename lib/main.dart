79aed9cf9a482187f1e3c8673189319bef91ad2d
gretosk <101219020+gretosk@users.noreply.github.com>
2022-04-05 00:19:29 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue.shade900,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/gretar.jpg'),
               ),
               const Text(
-                'Grétar Ó.',
+                'Grétar Óskarsson',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
@@ -35,41 +36,46 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'SourceSansPro-Regular',
                   fontSize: 10.0,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2,
+                  color: Colors.blue.shade100,
+                  letterSpacing: 3,
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 300.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
                     Icons.phone,
-                    color: Colors.teal,
+                    color: Colors.blue.shade800,
                   ),
                   title: Text(
                     '+354 853 8651',
                     style: TextStyle(
                         fontSize: 20.0,
-                        color: Colors.teal.shade900,
+                        color: Colors.blue.shade800,
                         fontFamily: 'Source Sans Pro'),
                   ),
                 ),
               ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                   child: ListTile(
                     leading: Icon(
-                      Icons.alternate_email,
-                      color: Colors.teal,
+                      Icons.mail,
+                      color: Colors.blue.shade800,
                     ),
                     title: Text(
                         'gretar.oskarsson@marel.com',
                     style: TextStyle(
                         fontSize: 20.0,
-                        color: Colors.teal.shade900,
+                        color: Colors.blue.shade800,
                         fontFamily: 'Source Sans Pro'),
                     ),
                 ),
